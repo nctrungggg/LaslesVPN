71530627d38ab08b8f7c3d24de5f4a1c17449864
Trung Nguyen <nctrung04102000@gmail.com>
2022-04-14 00:15:51 +0700

    one

---
+++ index.js
@@ -0,0 +1,50 @@
+"use strict";
+const header = document.querySelector(".header");
+const banner = document.querySelector(".banner");
+const btt = document.querySelector(".scroll-btn");
+
+// Sticky navigation: Intersection Observer API
+const navHeight = header.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) header.classList.add("sticky");
+  else header.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(banner);
+
+// Scroll btn back to top
+const displayBTT = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) btt.classList.add("show");
+  else btt.classList.remove("show");
+
+  btt.addEventListener("click", function (e) {
+    banner.scrollIntoView({ behavior: "smooth" });
+  });
+};
+
+const bttObserver = new IntersectionObserver(displayBTT, {
+  threshold: 0,
+});
+bttObserver.observe(banner);
+
+// Page navigation
+document.querySelector(".header-menu").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("header-menu-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id)?.scrollIntoView({ behavior: "smooth" });
+  }
+});
