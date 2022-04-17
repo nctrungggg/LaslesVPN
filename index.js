22eaa45d63d5c9ece105220e840dac30b0acd4ef
Trung Nguyen <nctrung04102000@gmail.com>
2022-04-17 12:20:13 +0700

    update menu-toggle

--- index.js
+++ index.js
@@ -49,3 +49,23 @@ document.querySelector(".header-menu").addEventListener("click", function (e) {
     document.querySelector(id)?.scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Menu toggle
+function handleToggleMenu() {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".header-menu");
+
+  if (!menuToggle || !menu) return;
+
+  menuToggle.addEventListener("click", function () {
+    menu.classList.add("is-active");
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+      menu.classList.remove("is-active");
+    }
+  });
+}
+
+handleToggleMenu();
